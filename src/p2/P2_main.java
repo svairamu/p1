f7e5f72df351e244b88206f33d27a9fe34305a55
Shatujan Vairamuthu <shatu@MacBook-Air-von-Shatujan.local>
2025-10-30 11:07:08 +0100

    Paul ist mein Onkel

---
+++ src/p2/P2_main.java
@@ -0,0 +1,12 @@
+package p2;
+
+public class P2_main {
+	public static void main(String[] args) {
+		int i = 11;
+		boolean b = false;
+		if (i > 10) {
+			b = true;
+		}
+		System.out.println(b);
+	}
+}
---
+++ src/p3/P3_Main.java
@@ -0,0 +1,19 @@
+package p3;
+
+public class P3_Main {
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 20;
+		int k = 30;
+		if (i < j) {
+			System.out.println("A");
+		}
+		if (i < k) {
+			System.out.println("B");
+		}
+		if (j < k) {
+			System.out.println("C");
+		}
+			
+	}
+}
